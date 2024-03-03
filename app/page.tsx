7416a94c33864cba1fa4a2e76a764c6b0364e54f
Dora <fly9687@gmail.com>
2024-03-03 14:34:56 -0800

    feat: add link from Home to About

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
       <main>
         <img src="/logo.svg" alt="shared logo" />
         <h1>Welcome to Shared!</h1>
-        <p> Let's share what you love to eat.</p>
+        <p>Let's share what you love to eat.</p>
+          <p><Link href="/about">About Us</Link></p>
       </main>
   );
 }
