2bd5a7e7a78ed495e799517e8f1650867bea251d
Dora <fly9687@gmail.com>
2024-03-03 15:18:42 -0800

    feat: add prettier and reformat Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
+import Header from '@/components/header';
 
 export default function Home() {
   return (
-      <main>
-        <img src="/logo.svg" alt="shared logo" />
-        <h1>Welcome to Shared!</h1>
-        <p>Let's share what you love to eat.</p>
-          <p><Link href="/about">About Us</Link></p>
-      </main>
+    <main>
+      <Header />
+      <p>Let's share what you love to eat.</p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
+    </main>
   );
 }
