bf55112955058fbdfb583d31a1c5c1aa3abb841f
Dora <fly9687@gmail.com>
2024-03-03 16:49:11 -0800

    feat: remove About Us page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Link from 'next/link';
 import Header from '@/components/header';
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <p>Let's share what you love to eat.</p>
-      <p>
-        <Link href="/about">About Us</Link>
-      </p>
     </main>
   );
 }
