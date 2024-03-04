bed87acf3c07894abe7ef14a8f7b7bde4084d33c
Dora <fly9687@gmail.com>
2024-03-03 22:33:47 -0800

    feat: add route pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Header from '@/components/header';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <p>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Share Meal</Link>
+      </p>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
     </main>
   );
 }
