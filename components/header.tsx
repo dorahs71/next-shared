eea807703ff38f6945dd3d0f234157e928db0ace
Dora <fly9687@gmail.com>
2024-03-04 23:30:51 -0800

    feat: add logo and nav links

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,27 @@
 import styles from './header.module.css';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <>
-      <div className={styles.logoWrapper}>
+    <header className={styles.header}>
+      <Link className={styles.logoWrapper} href="/">
         <img className={styles.logo} src="/logo.svg" alt="shared logo" />
         <div className={styles.logoText}>SHARED</div>
-      </div>
-    </>
+      </Link>
+
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Meals</Link>
+          </li>
+          <li>
+            <Link href="/meals/share">Share Meal</Link>
+          </li>
+          <li>
+            <Link href="/community">Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
