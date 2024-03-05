67d840c97a603f9c941cd549ca827a1d17853303
Dora <fly9687@gmail.com>
2024-03-04 23:30:12 -0800

    feat: move header from Hoe to Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Header from '@/components/header';
 
 export const metadata: Metadata = {
   title: 'Shared',
@@ -18,10 +19,11 @@ export default function RootLayout({
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
               fill="#F9A827"
-              d="M0,256L48,240C96,224,192,192,288,181.3C384,171,480,181,576,186.7C672,192,768,192,864,181.3C960,171,1056,149,1152,133.3C1248,117,1344,107,1392,101.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+              d="M 0 256 L 48 240 C 96 224 192 192 288 181.3 C 384 171 480 181 576 186.7 C 672 192 768 192 864 181.3 C 960 171 998 163 1058 167 C 1264 176 1301 194 1382 224 L 1440 256 L 1440 0 L 1392 0 C 1344 0 1248 0 1152 0 C 1056 0 960 0 864 0 C 768 0 672 0 576 0 C 480 0 384 0 288 0 C 192 0 96 0 48 0 L 0 0 Z"
             ></path>
           </svg>
         </div>
+        <Header />
         {children}
       </body>
     </html>
