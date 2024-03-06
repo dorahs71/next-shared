cbcf78caaefdf7c129d2fe90fc22c3de47e862f7
Dora <fly9687@gmail.com>
2024-03-05 22:52:46 -0800

    feat: add Home page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import styles from './page.module.css';
+import Link from 'next/link';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <main className={styles.intro}>
+        <div className={styles.slideshow}></div>
+        <div>
+          <div className={styles.hero}>
+            <h1>Enjoy NextLevel Food</h1>
+            <p>Taste & share food from all over the world!</p>
+          </div>
+          <div className={styles.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
