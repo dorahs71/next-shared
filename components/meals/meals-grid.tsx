bc5afc165bf25974e5ba5fad135f0deadf852079
Dora <fly9687@gmail.com>
2024-03-08 17:31:37 -0800

    feat: add MealGrid component

---
+++ components/meals/meals-grid.tsx
@@ -0,0 +1,15 @@
+import MealItem from './meal-item';
+import styles from './meals-grid.module.css';
+import { Meal } from '@meals/page';
+
+export default function MealsGrid({ meals }: { meals: Meal[] }) {
+  return (
+    <ul className={styles.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  );
+}
