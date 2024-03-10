f9ec6fb5f3e9c88ef9bdc8096ce6af494a5e02c0
Dora <fly9687@gmail.com>
2024-03-10 17:33:22 +0800

    feat: add getMealDetail function

--- lib/meals.ts
+++ lib/meals.ts
@@ -1,7 +1,12 @@
-import { db } from '../initdb';
+import { db } from '@/initdb';
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  // throw new Error('Loading eals Filed');
+  // throw new Error('Loading Meals Failed');
   return db.prepare('SELECT * FROM meals').all();
+}
+
+export async function getMealDetail(slug: string) {
+  // return db.prepare('SELECT * WHERE slug =' + slug); //have concern of db injection
+  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
