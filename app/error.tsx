149baf325e08434364613df70064892ef28dcfc0
Dora <fly9687@gmail.com>
2024-03-10 16:46:12 +0800

    feat: add error handling

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+export default function Error() {
+  return (
+    <div className={'error'}>
+      <h1>An Error Occurred!</h1>
+      <p>Failed to fetch meal data, please try again!</p>
+    </div>
+  );
+}
--- lib/meals.ts
+++ lib/meals.ts
@@ -2,5 +2,6 @@ import { db } from '../initdb';
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
+  // throw new Error('Loading eals Filed');
   return db.prepare('SELECT * FROM meals').all();
 }
