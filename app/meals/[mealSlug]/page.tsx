04ac397460f0062518efcf723c35d7de9c01a840
Dora <fly9687@gmail.com>
2024-03-10 17:33:36 +0800

    feat: add MealDetail page

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,3 +1,37 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import styles from './page.module.css';
+import { getMealDetail } from '@/lib/meals';
+import { Meal } from '@/initdb';
+import Image from 'next/image';
+
+export default async function MealDetailsPage({
+  params,
+}: {
+  params: { mealSlug: string };
+}) {
+  const meal: Meal = await getMealDetail(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <div>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </div>
+    </>
+  );
 }
