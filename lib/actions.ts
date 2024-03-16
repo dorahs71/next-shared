b2248cc60f49ea34769320288dbaf843ec0b6d9a
Dora <fly9687@gmail.com>
2024-03-16 11:18:49 +0800

    feat: translate formData to MealFormData and call saveMeal

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,31 @@
 'use server';
 
+import { saveMeal } from '@/lib/meals';
+
+export interface MealFormData {
+  title: string;
+  image: File;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
+}
+
 export async function shareMeal(formData: FormData) {
-  const meal = {
-    title: formData.get('title'),
-    image: formData.get('image'),
-    summary: formData.get('summary'),
-    instructions: formData.get('instructions'),
-    creator: formData.get('name'), // name of input
-    creator_email: formData.get('email'),
+  const title: string = (formData.get('title') as string) ?? '';
+  const summary: string = (formData.get('summary') as string) ?? '';
+  const instructions: string = (formData.get('instructions') as string) ?? '';
+  const creator: string = (formData.get('name') as string) ?? ''; // name of input
+  const creator_email: string = (formData.get('email') as string) ?? '';
+
+  const mealFormData: MealFormData = {
+    title,
+    image: formData.get('image') as File,
+    summary,
+    instructions,
+    creator,
+    creator_email,
   };
+
+  await saveMeal(mealFormData);
 }
