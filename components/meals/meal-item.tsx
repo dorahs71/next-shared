52d9d965cafeecacbd85cc4214856bd68f730e92
Dora <fly9687@gmail.com>
2024-03-24 21:54:05 +0800

    fix: adjust Meal import

--- components/meals/meal-item.tsx
+++ components/meals/meal-item.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import styles from './meal-item.module.css';
-import { Meal } from '@meals/page';
+import { Meal } from '@/initdb';
 
 export default function MealItem({ title, slug, image, summary, creator }: Meal) {
   return (
--- components/meals/meals-grid.tsx
+++ components/meals/meals-grid.tsx
@@ -1,12 +1,12 @@
 import MealItem from './meal-item';
 import styles from './meals-grid.module.css';
-import { Meal } from '@meals/page';
+import { Meal } from '@/initdb';
 
 export default function MealsGrid({ meals }: { meals: Meal[] }) {
   return (
     <ul className={styles.meals}>
       {meals.map((meal) => (
-        <li key={meal.id}>
+        <li key={meal.slug}>
           <MealItem {...meal} />
         </li>
       ))}
